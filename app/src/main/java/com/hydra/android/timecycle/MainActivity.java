ef479b850b35bf2c354f359ac78e2cccf8427384
[hydRa] <hydRwa@gmail.com>
2015-10-13 16:27:57 +0200

    StopWatch class created, Handler for UI update added

--- app/src/main/java/com/hydra/android/timecycle/MainActivity.java
+++ app/src/main/java/com/hydra/android/timecycle/MainActivity.java
@@ -1,15 +1,69 @@
 package com.hydra.android.timecycle;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "MainActivity";
+
+    // UI Components
+    private TextView textView_timeDisplay;
+    private Chronometer chronometer;
+    private ToggleButton startStopButton;
+    private Button lapReset;
+
+    private final int UI_REFRESH_RATE = 100;
+    private final int MSG_STOP_TIMER = 0;
+    private final int MSG_START_TIMER = 1;
+    private final int MSG_UPDATE_TIMER = 2;
+    private StopWatch stopWatch = new StopWatch();
+
+
+    // Handler refreshes UI with the stopwatch time by the given UI_REFRESH_RATE
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case MSG_START_TIMER:
+                    stopWatch.start();
+                    handler.sendEmptyMessage(MSG_UPDATE_TIMER);
+                    break;
+                case MSG_STOP_TIMER:
+                    handler.removeMessages(MSG_UPDATE_TIMER);
+                    stopWatch.stop();
+                    displayTime(formatTime(
+                            stopWatch.getElapsedTimeHours(),
+                            stopWatch.getElapsedTimeMinutes(),
+                            stopWatch.getElapsedTimeSecs(),
+                            stopWatch.getElapsedTimeMilli()));
+                    break;
+                case MSG_UPDATE_TIMER:
+                    displayTime(formatTime(
+                            stopWatch.getElapsedTimeHours(),
+                            stopWatch.getElapsedTimeMinutes(),
+                            stopWatch.getElapsedTimeSecs(),
+                            stopWatch.getElapsedTimeMilli()));
+                    handler.sendEmptyMessageDelayed(MSG_UPDATE_TIMER, UI_REFRESH_RATE);
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +80,11 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // Initialize UI components
+        startStop();
+        textView_timeDisplay = (TextView) findViewById(R.id.textView_timeDisplay);
+        lapReset();
     }
 
     @Override
@@ -49,4 +108,105 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    // Listener for the StartStop Button
+    private void startStop() {
+        startStopButton = (ToggleButton) findViewById(R.id.button_startStop);
+        //Create a reference to a listener to be sure that listener exist as long as activity does
+        CompoundButton.OnCheckedChangeListener listener = new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    start();
+                    lapReset.setText(R.string.Lap);
+                } else {
+                    stop();
+                    lapReset.setText(R.string.Reset);
+                }
+            }
+        };
+        startStopButton.setOnCheckedChangeListener(listener);
+    }
+
+    // Starts the StopWatch
+    private void start() {
+        Toast.makeText(MainActivity.this, "Start !", Toast.LENGTH_SHORT).show();
+        handler.sendEmptyMessage(MSG_START_TIMER);
+    }
+
+    // Stops the StopWatch
+    private void stop() {
+        Toast.makeText(MainActivity.this, "Stop !", Toast.LENGTH_SHORT).show();
+        handler.sendEmptyMessage(MSG_STOP_TIMER);
+    }
+
+    // Listener for the lapReset Button
+    private void lapReset() {
+        lapReset = (Button) findViewById(R.id.button_lapReset);
+        //Create a reference to a listener to be sure that listener exist as long as activity does
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (stopWatch.isStopped()) {
+                    reset();
+                } else {
+                    lap();
+                }
+            }
+        };
+        lapReset.setOnClickListener(listener);
+    }
+
+    // Laps the time
+    private void lap() {
+        Toast.makeText(MainActivity.this, "Lap !", Toast.LENGTH_SHORT).show();
+    }
+
+    // Resets the time
+    private void reset() {
+        Toast.makeText(MainActivity.this, "Reset !", Toast.LENGTH_SHORT).show();
+        stopWatch.resetTime();
+        textView_timeDisplay.setText(R.string.textView_default_time);
+    }
+
+    // Display the time
+    private void displayTime(String time) {
+        textView_timeDisplay.setText(time);
+    }
+
+    // Format the time
+    private String formatTime(long hour, long minute, long second, long milli) {
+        StringBuilder builder = new StringBuilder();
+        String hourString;
+        String minuteString;
+        String secondString;
+
+        if (hour < 10) {
+            hourString = "0" + hour;
+        } else {
+            hourString = "" + hour;
+        }
+
+        if (minute < 10) {
+            minuteString = "0" + minute;
+        } else {
+            minuteString = "" + minute;
+        }
+
+        if (second < 10) {
+            secondString = "0" + second;
+        } else {
+            secondString = "" + second;
+        }
+
+
+        builder.append(hourString + ":");
+        builder.append(minuteString + ":");
+        builder.append(secondString + ",");
+        builder.append(milli + "");
+
+        return builder.toString();
+    }
+
+
 }
---
+++ app/src/main/java/com/hydra/android/timecycle/StopWatch.java
@@ -0,0 +1,98 @@
+package com.hydra.android.timecycle;
+
+/**
+ * Created by jslapnicka on 12.10.2015.
+ */
+public class StopWatch {
+    private long startTime = 0;
+    private long endTime = 0;
+    private boolean isRunning = false;
+    private boolean isStopped = false;
+    private long currentTime = 0;
+
+    public StopWatch() {
+
+    }
+
+    public StopWatch(long startTime, long endTime) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public void start() {
+        if (!isStopped)
+            this.startTime = System.currentTimeMillis();
+        else {
+            this.startTime = System.currentTimeMillis() - endTime;
+        }
+        this.isRunning = true;
+        this.isStopped = false;
+    }
+
+    public void stop() {
+        this.endTime = System.currentTimeMillis() - startTime;
+        this.isRunning = true;
+        this.isStopped = true;
+    }
+
+    public void pause() {
+        this.isRunning = true;
+        currentTime = System.currentTimeMillis() - startTime;
+    }
+
+    public void resume() {
+        this.isRunning = true;
+        this.startTime = System.currentTimeMillis() - currentTime;
+    }
+
+    public boolean isRunning() {
+        return isRunning;
+    }
+
+    public boolean isStopped() {
+        return isStopped;
+    }
+
+    public void resetTime() {
+        startTime = 0;
+        endTime = 0;
+    }
+
+    // Returns time in milliseconds
+    public long getElapsedTimeMilli() {
+        long elapsed = 0;
+        if (isRunning) {
+            elapsed = ((System.currentTimeMillis() - startTime) / 100) % 10;
+        }
+        return elapsed;
+    }
+
+    // Returns time in seconds
+    public long getElapsedTimeSecs() {
+        long elapsed = 0;
+        if (isRunning) {
+            elapsed = ((System.currentTimeMillis() - startTime) / 1000) % 60;
+        }
+        return elapsed;
+    }
+
+    // Returns time in minutes
+    public long getElapsedTimeMinutes() {
+        long elapsed = 0;
+        if (isRunning) {
+            elapsed = (((System.currentTimeMillis() - startTime) / 1000) / 60) % 60;
+        }
+        return elapsed;
+    }
+
+    // Returns time in hours
+    public long getElapsedTimeHours() {
+        long elapsed = 0;
+        if (isRunning) {
+            // TODO: Check the mod 24 if working
+            elapsed = ((((System.currentTimeMillis() - startTime) / 1000) / 60) / 60) % 24;
+        }
+        return elapsed;
+    }
+
+}
